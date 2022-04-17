dd76374d654cbad060dbf772f8c6a842cd9dd5fb
vmercel <vmercel@gmail.com>
2022-04-17 15:21:54 +0100

    first commit

---
+++ public/storage_initialize.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// Set the configuration for your app
+// TODO: Replace with your app's config object
+const firebaseConfig = {
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    storageBucket: '<your-storage-bucket-url>'
+};
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const storage = getStorage(firebaseApp);
